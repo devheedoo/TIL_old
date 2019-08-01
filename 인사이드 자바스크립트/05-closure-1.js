f36241fb3f7d82fd859d7d375446a4fa79671d89
Heedo Kim <devheedoo@gmail.com>
2019-08-01 12:50:22 +0900

    05 실행 컨텍스트와 클로저 예제

---
+++ 인사이드 자바스크립트/05-closure-1.js
@@ -0,0 +1,41 @@
+function HelloFunc(func) {
+  this.greeting = 'hello';
+}
+
+// 파라미터 함수 호출, 없으면 this.func() 호출
+HelloFunc.prototype.call = function(paramFunc) {
+  paramFunc ? paramFunc(this.greeting) : this.func(this.greeting);
+}
+
+var userFunc = function(greeting) {
+  console.log(greeting);
+}
+
+var objHello = new HelloFunc();
+objHello.func = userFunc;
+
+objHello.call();
+
+//
+
+function saySomething(obj, methodName, name) {
+  return (function(greeting) {
+    return obj[methodName](greeting, name);
+  });
+}
+
+// 객체의 메소드에 파라미터로 함수 호출
+function newObj(obj, name) {
+  obj.func = saySomething(this, 'who', name);
+  return obj;
+}
+
+newObj.prototype.who = function(greeting, name) {
+  console.log(greeting + ' ' + (name || 'everyone'));
+}
+
+var obj1 = new newObj(objHello);
+// objHello.func = userFunc에서 saySomething으로 변경
+// saySomething은 who 함수 호출
+
+obj1.call();
---
+++ 인사이드 자바스크립트/05-closure-2.js
@@ -0,0 +1,24 @@
+function countSeconds(howMany) {
+  for (var i=1; i<=howMany; i++) {
+    console.log(`for loop(${i}):`, new Date());
+    setTimeout(function () {
+      console.log(i);
+    }, i*1000);
+  }
+}
+
+// countSeconds(3);
+
+function countSecondsClosure(howMany) {
+  for (var i=1; i<=howMany; i++) {
+    console.log(`for loop(${i}):`, new Date());
+    (function (copyI) {
+      console.log(`즉시실행함수(${copyI}):`, new Date());
+      setTimeout(function () {
+        console.log(copyI, new Date());
+      }, copyI*1000);
+    }(i))
+  }
+}
+
+countSecondsClosure(3);
